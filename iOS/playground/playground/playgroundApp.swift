1f2970a1489ceae99dd4ca0d8aa3b3acd03785f1
Hideyoshi Moriya <hide@piyo.jp>
2024-07-13 14:59:11 +0900

    add a new Xcode project

---
+++ iOS/playground/playground/playgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  playgroundApp.swift
+//  playground
+//
+//  Created by Hideyoshi Moriya on 2024/07/13.
+//
+
+import SwiftUI
+
+@main
+struct playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
