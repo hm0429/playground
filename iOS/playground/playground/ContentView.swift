231dd91c3f99b1ebfadd068b32d335707a00a792
Hideyoshi Moriya <hide@piyo.jp>
2024-07-13 15:23:34 +0900

    Update ContentView.swift

--- iOS/playground/playground/ContentView.swift
+++ iOS/playground/playground/ContentView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var showSheet = false
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Button(action: {
+                showSheet.toggle()
+            }, label: {
+                Text("今日の運勢は")
+            })
+            .sheet(isPresented: $showSheet){
+                ResultView()
+            }
         }
         .padding()
+    }
+}
+
+struct ResultView: View {
+    var body: some View {
+        if let unsei = ["ラーメン食べていい！", "大吉", "中吉", "吉"].randomElement() {
+            Text(unsei)
+        }
     }
 }
 
